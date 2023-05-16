14f95519d8451e952a3f4929f8b46e7e66fb9325
samy chabani <s.chabani13@gmail.com>
2023-05-16 12:50:13 +0000

    category formulaire et controller

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $catgeory = new Category();
+        $form = $this->createForm(CategoryType::class, $catgeory)->add('Save', SubmitType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($catgeory);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('category/index.html.twig', ['form' => $form]);
+    }
+}
