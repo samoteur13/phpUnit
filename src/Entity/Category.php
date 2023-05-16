144e8ea12a76b39e917d7911e70fc63d7a838ce0
samy chabani <s.chabani13@gmail.com>
2023-05-16 15:04:21 +0000

    correction bug strigify

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -26,6 +26,11 @@ class Category
         $this->books = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
